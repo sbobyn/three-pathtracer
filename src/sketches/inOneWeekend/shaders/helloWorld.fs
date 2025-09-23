3c1e8f7b8e9998dbab08cb1883bfe16022bf66b1
sbobyn <50857495+sbobyn@users.noreply.github.com>
2025-09-22 23:13:48 -0700

    added camera and background sky

--- src/sketches/inOneWeekend/shaders/helloWorld.fs
+++ src/sketches/inOneWeekend/shaders/helloWorld.fs
@@ -2,11 +2,30 @@ precision highp float;
 
 uniform vec2 uResolution;
 
+struct Ray {
+    vec3 origin;
+    vec3 direction;
+};
+
+vec3 rayColor(Ray r) {
+    vec3 unitDir = normalize(r.direction);
+    float a = 0.5 * (unitDir.y + 1.0);
+
+    return (1.0 - a) * vec3(1) + a * vec3(0.5, 0.7, 1);
+}
+
 void main() {
-    vec2 uv = gl_FragCoord.xy / uResolution;
-    uv.y = 1.0 - uv.y;
+    float focalLength = 1.0;
+    float aspect = uResolution.x / uResolution.y;
+    vec2 uv = (gl_FragCoord.xy / uResolution) * 2.0 - 1.0; // to ndc
+    uv.x *= aspect;
 
-    vec3 color = vec3(uv, 0);
+    vec3 cameraPos = vec3(0);
 
+    vec3 rayDir = vec3(uv, focalLength) - cameraPos;
+
+    Ray r = Ray(cameraPos, rayDir);
+
+    vec3 color = rayColor(r);
     gl_FragColor = vec4(color, 1.0);
 }
