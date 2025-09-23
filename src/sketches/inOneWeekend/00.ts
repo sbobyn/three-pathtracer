79f01a7b3029a7cbc2d052271eabd699b1d86a4d
sbobyn <50857495+sbobyn@users.noreply.github.com>
2025-09-22 23:14:40 -0700

    rename to main.fs

--- src/sketches/inOneWeekend/00.ts
+++ src/sketches/inOneWeekend/00.ts
@@ -1,6 +1,6 @@
 import * as THREE from "three";
 import { ShaderCanvas } from "../../core/ShaderCanvas";
-import fragShader from "./shaders/helloWorld.fs";
+import fragShader from "./shaders/main.fs";
 
 export default function (): THREE.WebGLRenderer {
   const shaderDemo = new ShaderCanvas({
